c0e08b6cd7d9a2cd961e9d8f56d75deb969de81c
brunnoterada <brunoterada99@gmail.com>
2021-06-29 20:20:07 -0300

    quiz finish

--- quiz/script.js
+++ quiz/script.js
@@ -32,3 +32,61 @@ const quizData = [
     correct: "b",
   },
 ];
+const cabeca = document.getElementById('quiz-header');
+const respostasEls = document.querySelectorAll('.resposta');
+const questaoEl = document.getElementById('questaoTitle');
+const a_texto = document.getElementById('a_texto');
+const b_texto = document.getElementById('b_texto');
+const c_texto = document.getElementById('c_texto');
+const d_texto = document.getElementById('d_texto');
+const submitBtn = document.getElementById('enviar')
+
+let QuestaoAtual= 0;
+let resultado = 0;
+
+loadQuiz();
+
+
+function loadQuiz() {
+  deselectResposta();
+  const QuestaoAtualDados = quizData[QuestaoAtual];
+  questaoEl.innerText = QuestaoAtualDados.questao;
+  a_texto.innerText=QuestaoAtualDados.a;
+  b_texto.innerText=QuestaoAtualDados.b;
+  c_texto.innerText=QuestaoAtualDados.c;
+  d_texto.innerText=QuestaoAtualDados.d;
+
+}
+
+function getSelected(){
+  let resposta = undefined; 
+  respostasEls.forEach((respostaEl)=>{
+    if(respostaEl.checked){
+      resposta = respostaEl.id;
+    }
+  });
+  return resposta;
+}
+function deselectResposta(){
+  respostasEls.forEach((respostaEl)=>{
+    respostaEl.checked=false;
+  });
+  
+
+}
+
+submitBtn.addEventListener('click',()=>{
+
+  const resposta = getSelected();
+if(resposta){
+  if(resposta === quizData[QuestaoAtual].correct){
+    resultado++;
+  }
+  QuestaoAtual++
+    if(QuestaoAtual< quizData.length){
+        loadQuiz();
+    }else{
+        cabeca.innerHTML = `<h2>Você acertou ${resultado}/${quizData.length} </h2>`;
+    }
+  } 
+});
