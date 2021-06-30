aa5e3b3106f28fa5b114a18d59f7ea6b62c296cd
brunnoterada <brunoterada99@gmail.com>
2021-06-30 09:19:45 -0300

    ajustes no quiz

--- quiz/script.js
+++ quiz/script.js
@@ -1,6 +1,6 @@
 const quizData = [
   {
-    questao: "Quantos anos eu tem?",
+    questao: "Quantos anos eu tenho?",
     a: "20",
     b: "21",
     c: "22",
@@ -32,61 +32,56 @@ const quizData = [
     correct: "b",
   },
 ];
-const cabeca = document.getElementById('quiz-header');
-const respostasEls = document.querySelectorAll('.resposta');
-const questaoEl = document.getElementById('questaoTitle');
-const a_texto = document.getElementById('a_texto');
-const b_texto = document.getElementById('b_texto');
-const c_texto = document.getElementById('c_texto');
-const d_texto = document.getElementById('d_texto');
-const submitBtn = document.getElementById('enviar')
+const cabeca = document.getElementById("quiz-header");
+const respostasEls = document.querySelectorAll(".resposta");
+const questaoEl = document.getElementById("questaoTitle");
+const a_texto = document.getElementById("a_texto");
+const b_texto = document.getElementById("b_texto");
+const c_texto = document.getElementById("c_texto");
+const d_texto = document.getElementById("d_texto");
+const submitBtn = document.getElementById("enviar");
 
-let QuestaoAtual= 0;
+let QuestaoAtual = 0;
 let resultado = 0;
 
 loadQuiz();
-
 
 function loadQuiz() {
   deselectResposta();
   const QuestaoAtualDados = quizData[QuestaoAtual];
   questaoEl.innerText = QuestaoAtualDados.questao;
-  a_texto.innerText=QuestaoAtualDados.a;
-  b_texto.innerText=QuestaoAtualDados.b;
-  c_texto.innerText=QuestaoAtualDados.c;
-  d_texto.innerText=QuestaoAtualDados.d;
-
+  a_texto.innerText = QuestaoAtualDados.a;
+  b_texto.innerText = QuestaoAtualDados.b;
+  c_texto.innerText = QuestaoAtualDados.c;
+  d_texto.innerText = QuestaoAtualDados.d;
 }
 
-function getSelected(){
-  let resposta = undefined; 
-  respostasEls.forEach((respostaEl)=>{
-    if(respostaEl.checked){
+function getSelected() {
+  let resposta = undefined;
+  respostasEls.forEach((respostaEl) => {
+    if (respostaEl.checked) {
       resposta = respostaEl.id;
     }
   });
   return resposta;
 }
-function deselectResposta(){
-  respostasEls.forEach((respostaEl)=>{
-    respostaEl.checked=false;
+function deselectResposta() {
+  respostasEls.forEach((respostaEl) => {
+    respostaEl.checked = false;
   });
-  
-
 }
 
-submitBtn.addEventListener('click',()=>{
-
+submitBtn.addEventListener("click", () => {
   const resposta = getSelected();
-if(resposta){
-  if(resposta === quizData[QuestaoAtual].correct){
-    resultado++;
-  }
-  QuestaoAtual++
-    if(QuestaoAtual< quizData.length){
-        loadQuiz();
-    }else{
-        cabeca.innerHTML = `<h2>Você acertou ${resultado}/${quizData.length} </h2>`;
+  if (resposta) {
+    if (resposta === quizData[QuestaoAtual].correct) {
+      resultado++;
     }
-  } 
+    QuestaoAtual++;
+    if (QuestaoAtual < quizData.length) {
+      loadQuiz();
+    } else {
+      cabeca.innerHTML = `<h2>Você acertou ${resultado}/${quizData.length} </h2><button onclick="location.reload()">Reload</button>`;
+    }
+  }
 });
